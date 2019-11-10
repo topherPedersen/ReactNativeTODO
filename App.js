9b5d3ae1e6ac25eae51996bcff16e1515cf5d777
Christopher Pedersen <chris@topherpedersen.com>
2019-11-10 05:38:04 -0600

    added TextInput

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TextInput } from 'react-native';
 import List from './List';
 
 class App extends Component {
@@ -7,8 +7,16 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: ["foo", "bar", "baz"]
+      items: ["foo", "bar", "baz"],
+      text: 'yeet'
     };
+  }
+
+  handleOnChangeText(text) {
+    var oldState = this.state;
+    var newState = oldState;
+    newState.text = text;
+    this.setState(newState);
   }
 
   // TODO: create addItemToList() function
@@ -18,8 +26,19 @@ class App extends Component {
   render() {
     return (
       <View>
+
         <Text>ReactNativeTODO</Text>
+
         <List items={ this.state.items } />
+
+        <Text>----------------------</Text>
+        <Text>{this.state.text}</Text>
+        <Text>----------------------</Text>
+
+        <TextInput
+          placeholder="Enter TODO List Items Here"
+          onChangeText={ (text) => this.handleOnChangeText(text) }/>
+
       </View>
     );
   }
